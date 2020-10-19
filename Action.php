516e9d45f55af15acd912181c71ac43e673b043a
Mcjokki <72808096+Mcjokki@users.noreply.github.com>
2020-10-19 21:30:12 +0200

    Add files via upload

---
+++ Action.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Action</title>
+    <link rel="stylesheet" href="Action.Css.style.css">
+
+    <style>
+     
+        
+
+
+    </style>
+
+</head>
+<body>
+
+<div class="conteiner2">
+
+
+
+<header>
+
+<img src="img/supernatural-logo.jpg" alt="Logo for supernatural">
+</header>
+
+<nav>
+    <a href="Drama.html">Drama</a>
+    <a href="C:\Users\mcjok\Desktop\Prosjekt 1\Index.html">Hovedmeny</a>
+    <a href="komiserie.html">Komiserie</a>
+</nav>
+
+<main>
+<h1>Supernatural</h1>
+<h2><p>Supernatural følger brødrene Dean og Sam Winchester. Det er hele 15 sesonger laget av serien som handler om at brødrene kjemper mot overnaturlige vesener. Alt av myter og legender er med i denne serien, alle religioner er sanne så det er mye å ta av. I serien reiser brødrene rundt finner ut hva som plager området og finner ut hvordan de kan kvitte seg med det. En av metodene de bruker for å finne informasjon er å bruke falske politi, FBI o,l skilter. De kan også finne på å fake andre typer jobber for å finne ut mest mulig. Noen av sesongene er fantastisk bra mens andre går sakte noe som kan til tider gjøre det vanskelig.  I løpet av serien har de et par episoder som er laget for å være morsomme og disse er en fryd å se på. Dean er den eldste og følger farens ordre til punkt og prikke. Sam ser seg lei av livet som jakter (det folk som jakter overnaturlige skapninger kalles.) så Sam begynner å studere Jus på Stanford universitet. Dean henter broren sin fordi faren har vært borte lenge, så de drar sammen for å finne han. Serien har mange bra høydepunkter og alle episodene har action. Sam og Dean er 2 fantastiske karakterer å følge. Mange og karakterene du møter i løpet av serien er også bra laget. 
+    Alt i alt er det en 5er men av de 15 sesongene så har de et par dårlige, men 15 sesonger er mye tid foran skjermen så alt kan ikke være like bra.
+    </p></h2>
+
+</main>
+    
+</div>    
+    
+
+
+
+</body>
+</html>
